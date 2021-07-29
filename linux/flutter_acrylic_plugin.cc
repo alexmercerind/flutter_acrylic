6cab530d09268779e530927e1438ea371bdb2ce0
alexmercerind <saini123hitesh@gmail.com>
2021-07-29 15:37:38 +0530

    [plugin] Initial Linux support

--- linux/flutter_acrylic_plugin.cc
+++ linux/flutter_acrylic_plugin.cc
@@ -1,39 +1,54 @@
 #include "include/flutter_acrylic/flutter_acrylic_plugin.h"
-
 #include <flutter_linux/flutter_linux.h>
-#include <gtk/gtk.h>
-#include <sys/utsname.h>
-
 #include <cstring>
 
-#define FLUTTER_ACRYLIC_PLUGIN(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj), flutter_acrylic_plugin_get_type(), \
-                              FlutterAcrylicPlugin))
+#define FLUTTER_ACRYLIC_PLUGIN(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), flutter_acrylic_plugin_get_type(), FlutterAcrylicPlugin))
+
+#include <gtk/gtk.h>
+#include <cairo/cairo.h>
 
 struct _FlutterAcrylicPlugin {
   GObject parent_instance;
 };
 
+static double R = 0.0, G = 0.0, B = 0.0, A = 0.0;
+static FlPluginRegistrar* g_registrar = nullptr;
+
 G_DEFINE_TYPE(FlutterAcrylicPlugin, flutter_acrylic_plugin, g_object_get_type())
 
-// Called when a method call is received from Flutter.
-static void flutter_acrylic_plugin_handle_method_call(
-    FlutterAcrylicPlugin* self,
-    FlMethodCall* method_call) {
+gboolean drawCallback(GtkWidget* widget, cairo_t* cr, gpointer data) {
+  cairo_save(cr);
+  cairo_set_source_rgba(cr, R, G, B, A);
+  cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+  cairo_paint(cr);
+  cairo_restore(cr);
+  return FALSE;
+}
+
+static void flutter_acrylic_plugin_handle_method_call(FlutterAcrylicPlugin* self, FlMethodCall* method_call) {
   g_autoptr(FlMethodResponse) response = nullptr;
-
   const gchar* method = fl_method_call_get_name(method_call);
-
-  if (strcmp(method, "getPlatformVersion") == 0) {
-    struct utsname uname_data = {};
-    uname(&uname_data);
-    g_autofree gchar *version = g_strdup_printf("Linux %s", uname_data.version);
-    g_autoptr(FlValue) result = fl_value_new_string(version);
-    response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-  } else {
+  if (strcmp(method, "Acrylic.initialize") == 0) {
+    response = FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
+  }
+  if (strcmp(method, "Acrylic.setEffect") == 0) {
+    FlView* view = fl_plugin_registrar_get_view(g_registrar);
+    GtkWindow* window = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+    gtk_widget_hide(GTK_WIDGET(window));
+    gtk_widget_hide(GTK_WIDGET(view));
+    auto arguments = fl_method_call_get_args(method_call);
+    auto gradientColor = fl_value_lookup_string(arguments, "gradientColor");
+    R = ((double)fl_value_get_int(fl_value_lookup_string(gradientColor, "R")) / 255.0);
+    G = ((double)fl_value_get_int(fl_value_lookup_string(gradientColor, "G")) / 255.0);
+    B = ((double)fl_value_get_int(fl_value_lookup_string(gradientColor, "B")) / 255.0);
+    A = ((double)fl_value_get_int(fl_value_lookup_string(gradientColor, "A")) / 255.0);
+    gtk_widget_show(GTK_WIDGET(window));
+    gtk_widget_show(GTK_WIDGET(view));
+    response = FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
+  }
+  else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
-
   fl_method_call_respond(method_call, response, nullptr);
 }
 
@@ -47,24 +62,38 @@ static void flutter_acrylic_plugin_class_init(FlutterAcrylicPluginClass* klass) 
 
 static void flutter_acrylic_plugin_init(FlutterAcrylicPlugin* self) {}
 
-static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call,
-                           gpointer user_data) {
+static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call, gpointer user_data) {
   FlutterAcrylicPlugin* plugin = FLUTTER_ACRYLIC_PLUGIN(user_data);
   flutter_acrylic_plugin_handle_method_call(plugin, method_call);
 }
 
 void flutter_acrylic_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
-  FlutterAcrylicPlugin* plugin = FLUTTER_ACRYLIC_PLUGIN(
-      g_object_new(flutter_acrylic_plugin_get_type(), nullptr));
-
+  FlutterAcrylicPlugin* plugin = FLUTTER_ACRYLIC_PLUGIN(g_object_new(flutter_acrylic_plugin_get_type(), nullptr));
   g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
-  g_autoptr(FlMethodChannel) channel =
-      fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
-                            "flutter_acrylic",
-                            FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(channel, method_call_cb,
-                                            g_object_ref(plugin),
-                                            g_object_unref);
-
+  g_autoptr(FlMethodChannel) channel = fl_method_channel_new(
+    fl_plugin_registrar_get_messenger(registrar),
+    "flutter_acrylic",
+    FL_METHOD_CODEC(codec)
+  );
+  fl_method_channel_set_method_call_handler(
+    channel,
+    method_call_cb,
+    g_object_ref(plugin),
+    g_object_unref
+  );
+  FlView* view = fl_plugin_registrar_get_view(registrar);
+  GtkWindow* window = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+  GdkScreen* screen;
+  GdkVisual* visual;
+  gtk_widget_set_app_paintable(GTK_WIDGET(window), TRUE);
+  screen = gdk_screen_get_default();
+  visual = gdk_screen_get_rgba_visual(screen);
+  if (visual != NULL && gdk_screen_is_composited(screen)) {
+    gtk_widget_set_visual(GTK_WIDGET(window), visual);
+  }
+  g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(drawCallback), NULL);
+  gtk_widget_show(GTK_WIDGET(window));
+  gtk_widget_show(GTK_WIDGET(view));
+  g_registrar = registrar;
   g_object_unref(plugin);
 }
