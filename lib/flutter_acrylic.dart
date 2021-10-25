0674db6b356c2150ba7efe804e1f314b88bce261
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-10-25 23:53:25 +0530

    Update Dart side code

--- lib/flutter_acrylic.dart
+++ lib/flutter_acrylic.dart
@@ -1,103 +1,142 @@
+library flutter_acrylic;
+
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
-final MethodChannel _channel = const MethodChannel('flutter_acrylic');
-final Completer<void> _create = new Completer<void>();
+/// Platform channel name.
+const _kChannelName = "com.alexmercerind/flutter_acrylic";
 
-/// Acrylic effects for the Flutter window.
-enum AcrylicEffect {
-  /// Default window background. Works on Linux.
+/// Initializes the plugin.
+const _kInitialize = "Initialize";
+
+/// Sets window effect.
+const _kSetEffect = "SetEffect";
+
+/// Hides window controls
+const _kHideWindowControls = "HideWindowControls";
+
+/// Shows window controls
+const _kShowWindowControls = "ShowWindowControls";
+
+/// Enters fullscreen.
+const _kEnterFullscreen = "EnterFullscreen";
+
+/// Exits fullscreen.
+const _kExitFullscreen = "ExitFullscreen";
+
+final MethodChannel _kChannel = const MethodChannel(_kChannelName);
+final Completer<void> _kCompleter = new Completer<void>();
+
+/// Available effects for the Flutter window.
+enum WindowEffect {
+  /// Default window background.
+  /// Works on Windows & Linux.
   disabled,
 
-  /// Solid window background. Works on Linux.
+  /// Solid colored window background.
+  /// Works on Windows & Linux.
   solid,
 
-  /// Transparent window background. Works on Linux.
+  /// Transparent window background.
+  /// Works on Windows & Linux.
   transparent,
 
-  /// Aero blur effect. Windows Vista & Windows 7 like glossy blur effect.
+  /// Aero glass effect.
+  /// Windows Vista & Windows 7 like glossy blur effect.
+  /// Works only on Windows.
   aero,
 
-  /// Acrylic blur effect. Requires Windows 10 version 1803 or higher.
-  acrylic
-}
+  /// Acrylic is a type of brush that creates a translucent texture. You can apply acrylic to app surfaces to add depth and help establish a visual hierarchy.
+  /// Works only on Windows 10 version 1803 or higher.
+  acrylic,
 
-/// **Acrylic**
-///
-/// _Example_
-/// ```dart
-/// Future<void> main() async {
-///   WidgetsFlutterBinding.ensureInitialized();
-///   await Acrylic.initialize();
-///   runApp(MyApp());
-/// }
-///
-/// ...
-/// await Acrylic.setEffect(
-///   effect: AcrylicEffect.aero,
-///   gradientColor: Colors.white.withOpacity(0.2)
-/// );
-/// ```
-///
-class Acrylic {
-  /// Initializes [Acrylic] class.
-  ///
-  /// Must be called before calling [Acrylic.setEffect].
-  ///
-  /// _Example_
-  /// ```dart
-  /// await Acrylic.initialize();
-  /// ```
-  ///
-  static Future<void> initialize({bool drawCustomFrame: false}) async {
-    await _channel.invokeMethod(
-        'Acrylic.initialize', {'drawCustomFrame': drawCustomFrame});
-    _create.complete();
-  }
-
-  /// Sets [BlurEffect] for the window.
-  ///
-  /// Uses undocumented `SetWindowCompositionAttribute` API from `user32.dll` on Windows.
-  ///
-  /// Enables aero, acrylic or other transparency on the Flutter instance window.
-  ///
-  /// _Example_
-  /// ```dart
-  /// await FlutterAcrylic.setEffect(
-  ///   effect: AcrylicEffect.acrylic,
-  ///   gradientColor: Colors.black.withOpacity(0.2)
-  /// );
-  /// ```
-  ///
-  static Future<void> setEffect(
-      {required AcrylicEffect effect,
-      Color gradientColor: Colors.white}) async {
-    await _create.future;
-    await _channel.invokeMethod('Acrylic.setEffect', {
-      'effect': effect.index,
-      'gradientColor': {
-        'R': gradientColor.red,
-        'G': gradientColor.green,
-        'B': gradientColor.blue,
-        'A': gradientColor.alpha
-      }
-    });
-  }
+  /// Mica is an opaque, dynamic material that incorporates theme and desktop wallpaper to paint the background of long-lived windows.
+  /// Works only on Windows 11 or greater.
+  mica
 }
 
 /// **Window**
 ///
-/// General utilities to control Flutter instance window.
+/// Primary class to control Flutter instance window.
 ///
 class Window {
+  /// Initializes the [Window] class.
+  ///
+  /// _Example_
+  /// ```dart
+  /// Future<void> main() async {
+  ///   WidgetsFlutterBinding.ensureInitialized();
+  ///   await Window.initialize();
+  ///   runApp(MyApp());
+  /// }
+  /// ```
+  static Future<void> initialize() async {
+    await _kChannel.invokeMethod(_kInitialize);
+    _kCompleter.complete();
+  }
+
+  /// Sets specified effect for the window.
+  ///
+  /// When using [WindowEffect.mica], [dark] argument can be used to switch between light or dark mode of Mica.
+  ///
+  /// When using [WindowEffect.acrylic], [WindowEffect.aero], [WindowEffect.disabled], [WindowEffect.solid] or [WindowEffect.transparent],
+  /// [color] argument can be used to change the resulting tint (or color) of the window background.
+  ///
+  /// _Examples_
+  ///
+  /// ```dart
+  /// await Window.setEffect(
+  ///   effect: WindowEffect.acrylic,
+  ///   color: Color(0xCC222222),
+  /// );
+  /// ```
+  ///
+  /// ```dart
+  /// await Window.setEffect(
+  ///   effect: WindowEffect.mica,
+  ///   dark: true,
+  /// );
+  /// ```
+  ///
+  static Future<void> setEffect({
+    required WindowEffect effect,
+    Color color: Colors.transparent,
+    bool dark: true,
+  }) async {
+    await _kCompleter.future;
+    await _kChannel.invokeMethod(
+      _kSetEffect,
+      {
+        'effect': effect.index,
+        'color': {
+          'R': color.red,
+          'G': color.green,
+          'B': color.blue,
+          'A': color.alpha,
+        },
+        'dark': dark,
+      },
+    );
+  }
+
+  /// Hides window controls.
+  static Future<void> hideWindowControls() async {
+    await _kChannel.invokeMethod(_kHideWindowControls);
+  }
+
+  /// Shows window controls.
+  static Future<void> showWindowControls() async {
+    await _kChannel.invokeMethod(_kShowWindowControls);
+  }
+
   /// Makes the Flutter window fullscreen.
   static Future<void> enterFullscreen() async {
-    await _channel.invokeMethod('Window.enterFullscreen');
+    await _kChannel.invokeMethod(_kEnterFullscreen);
   }
 
   /// Restores the Flutter window back to normal from fullscreen mode.
   static Future<void> exitFullscreen() async {
-    await _channel.invokeMethod('Window.exitFullscreen');
+    await _kChannel.invokeMethod(_kExitFullscreen);
   }
 }
