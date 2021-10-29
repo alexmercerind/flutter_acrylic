7b88113f7967ec7a5e51c8c2bcf491531dc4a249
Adrian Samoticha <adrian@samoticha.de>
2021-10-29 12:47:27 +0200

    Example: Add macOS support

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
+import flutter_acrylic
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  FlutterAcrylicPlugin.register(with: registry.registrar(forPlugin: "FlutterAcrylicPlugin"))
 }
--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import flutter_acrylic
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -9,6 +10,21 @@ class MainFlutterWindow: NSWindow {
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
+    
+    /* Hide the window titlebar */
+    self.titleVisibility = NSWindow.TitleVisibility.hidden;
+    self.titlebarAppearsTransparent = true;
+    self.isMovableByWindowBackground = true;
+    self.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)?.isEnabled = false;
+
+    /* Make the window transparent */
+    self.isOpaque = false
+    self.backgroundColor = .clear
+    
+    /* Initialize the flutter_acrylic plugin */
+    let contentView = contentViewController!.view;
+    MainFlutterWindowManipulator.setContentView(contentView: contentView)
+    MainFlutterWindowManipulator.setInvalidateShadowFunction(invalidateShadow: self.invalidateShadow)
 
     super.awakeFromNib()
   }
