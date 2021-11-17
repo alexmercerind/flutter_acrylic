0141c0684b990e45c79affb833d205bf78c984ff
Adrian Samoticha <86920182+Adrian-Samoticha@users.noreply.github.com>
2021-11-18 00:04:21 +0100

    Example: Update MainFlutterWindow.swift

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -4,27 +4,15 @@ import flutter_acrylic
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
-    self.contentViewController = flutterViewController
+    let blurryContainerViewController = BlurryContainerViewController() // new
+    self.contentViewController = blurryContainerViewController // new
     self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
-    
-    /* Hide the window titlebar */
-    self.titleVisibility = NSWindow.TitleVisibility.hidden;
-    self.titlebarAppearsTransparent = true;
-    self.isMovableByWindowBackground = true;
-    self.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)?.isEnabled = false;
-
-    /* Make the window transparent */
-    self.isOpaque = false
-    self.backgroundColor = .clear
     
     /* Initialize the flutter_acrylic plugin */
-    let contentView = contentViewController!.view;
-    MainFlutterWindowManipulator.setContentView(contentView: contentView)
-    MainFlutterWindowManipulator.setInvalidateShadowFunction(invalidateShadow: self.invalidateShadow)
+    MainFlutterWindowManipulator.start(mainFlutterWindow: self) // new
+    
+    RegisterGeneratedPlugins(registry: blurryContainerViewController.flutterViewController) // new
 
     super.awakeFromNib()
   }
