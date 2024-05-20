9191fc38ddfb9b6c640419f817ef000ed6ea73a4
Alessandro Autiero <alautiero@gmail.com>
2024-05-20 17:23:59 +0200

    9.0.2

--- lib/flutter_acrylic.dart
+++ lib/flutter_acrylic.dart
@@ -1,8 +1,8 @@
 library flutter_acrylic;
 
-export 'widgets/titlebar_safe_area.dart';
 export 'macos/macos_blur_view_state.dart';
 export 'macos/macos_toolbar_style.dart';
 export 'macos/visual_effect_view_properties.dart';
-export 'window_effect.dart';
+export 'widgets/titlebar_safe_area.dart';
 export 'window.dart';
+export 'window_effect.dart';
